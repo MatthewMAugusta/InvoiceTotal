2449b19f4a3eca08c9818742664a180a8a8e127a
MATTcrow55 <130786720+MATTcrow55@users.noreply.github.com>
2026-02-04 16:29:51 -0500

    Add project files.

---
+++ InvoiceTotal/frmInvoiceTotal.cs
@@ -0,0 +1,15 @@
+namespace InvoiceTotal
+{
+    public partial class frmInvoiceTotal : Form
+    {
+        public frmInvoiceTotal()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
